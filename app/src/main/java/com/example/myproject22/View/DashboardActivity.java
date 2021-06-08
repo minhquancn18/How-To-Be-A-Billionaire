ce8640b625a343049aeafaf9f663868816cb846d
Tâm Trần Đức <19522166@ms.uit.edu.vn>
2021-06-09 01:46:50 +0700

    Done UI dashboard ?

--- app/src/main/java/com/example/myproject22/View/DashboardActivity.java
+++ app/src/main/java/com/example/myproject22/View/DashboardActivity.java
@@ -7,29 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.myproject22.R;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.snackbar.Snackbar;
 
+import io.alterac.blurkit.BlurKit;
 import io.alterac.blurkit.BlurLayout;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    BlurLayout blurLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
 
-        BlurLayout blurLayout = findViewById(R.id.blurLayout);
+        BlurKit.init(this);
+
+
+
+        blurLayout = findViewById(R.id.blurLayout);
         blurLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(DashboardActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
 
@@ -37,5 +45,17 @@ public class DashboardActivity extends AppCompatActivity {
     public void AddOnClicked(View view) {
         Intent intent = new Intent(this, AddingActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        blurLayout.startBlur();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        blurLayout.pauseBlur();
+        super.onStop();
     }
 }
