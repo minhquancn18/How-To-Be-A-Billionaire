641a8a9211bf7cd7ce54c9ac222db6c5bd98cb16
Tâm Trần Đức <19522166@ms.uit.edu.vn>
2021-06-17 18:25:33 +0700

    Add RecordDetailActivity

---
+++ RECORDDETAIL/app/src/main/java/com/example/myproject22/View/Activity/RecordDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.myproject22.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myproject22.R;
+
+public class RecordDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_detail);
+    }
+}
